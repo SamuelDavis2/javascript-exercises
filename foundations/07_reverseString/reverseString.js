390e582a2a29b8dfc736583ccd239306c6ea74b4
SamuelDavis2 <samueldavis531@gmail.com>
2026-04-16 00:50:31 +1000

    Tasks 6,7 and half of 8 done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = Array.from(str);
+    arr.reverse();
+    str = arr.join('');
+    return str;
 };
 
 // Do not edit below this line
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    for (let i = 0; i < arr.length; i++) {
+        let index = arr.indexOf(num[i]); 
+        if (index != -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
