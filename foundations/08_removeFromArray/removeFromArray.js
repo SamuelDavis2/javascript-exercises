2460f4921493fb7ff187792e0a6b0c114e7ea6d8
SamuelDavis2 <samueldavis531@gmail.com>
2026-04-16 18:13:17 +1000

    completing up to task 11

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,11 +1,5 @@
 const removeFromArray = function(arr, ...num) {
-    for (let i = 0; i < arr.length; i++) {
-        let index = arr.indexOf(num[i]); 
-        if (index != -1) {
-            arr.splice(index, 1);
-        }
-    }
-    return arr;
+    return arr.filter(item => !num.includes(item));
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 === 'string' || typeof num2 === 'string' || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    } else {
+        let arr = [];
+        for (let i = num1; i <= num2; i++) {
+         arr.push(i);
+        }
+        for (let i = num2; i <= num1; i++) {
+            arr.push(i);
+        }
+        let sum = arr.reduce((total, item) => total + item, 0);
+        return sum;
+    }
 };
 
 // Do not edit below this line
